6eaec7ab30d9d5919eb953d5eea9959b957720d0
Marharyta Skliarenko <marharyta.skliarenko@MediaKind.com>
2019-06-28 16:57:03 +0300

    New structure directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HeaderComponent } from './header/header.component';
 import { UserCardComponent } from './header/user-card/user-card.component';
 import { ItemComponent } from './item/item.component';
 import { ColoryDirective } from './colory.directive';
+import { DelayDirective } from './delay.directive';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { ColoryDirective } from './colory.directive';
     HeaderComponent,
     UserCardComponent,
     ItemComponent,
-    ColoryDirective
+    ColoryDirective,
+    DelayDirective
   ],
   imports: [
     BrowserModule,
