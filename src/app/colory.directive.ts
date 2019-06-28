46a21dd7b8e2da29e4615ec47b4427e4044853f4
Marharyta Skliarenko <marharyta.skliarenko@MediaKind.com>
2019-06-28 17:12:30 +0300

    Export controller og directive

--- src/app/colory.directive.ts
+++ src/app/colory.directive.ts
@@ -1,7 +1,8 @@
 import { Directive, HostBinding, HostListener,  } from '@angular/core';
 
 @Directive({
-  selector: '[appColory]'
+  selector: '[appColory]',
+  exportAs: 'colory'
 })
 export class ColoryDirective {
 
@@ -11,9 +12,13 @@ export class ColoryDirective {
   @HostBinding('style.color') myColor: string;
   // обработка соб-й
   @HostListener('click', ['$event']) changeColor(event) {
-    this.myColor = '#' + Math.floor(Math.random() * 167777215).toString(16);
+    this.setRandomColor();
     this.counter++;
     console.log(this.counter);
+  }
+
+  setRandomColor(){
+    this.myColor = '#' + Math.floor(Math.random() * 167777215).toString(16);
   }
 
   constructor() { 
